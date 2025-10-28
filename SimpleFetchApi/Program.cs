260aebf78a627bdc20a91e06f412eb113e10c396
renanwiezel <renanwiezel@gmail.com>
2025-10-28 09:22:30 -0300

    alterações nos headers pra tentar deixar natural e um delay de request aleatório

--- SimpleFetchApi/Program.cs
+++ SimpleFetchApi/Program.cs
@@ -86,14 +86,22 @@ public class Request
         var client = new HttpClient(handler);
 
         client.Timeout = TimeSpan.FromSeconds(30);
-        client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
-        //client.DefaultRequestHeaders.Connection.ParseAdd("keep-alive");
-        client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("pt-BR,pt;q=0.9");
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml") { Quality = 0.9 });
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*") { Quality = 0.8 });
+        // Headers mais completos para simular um browser real
+        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+        client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8");
+        client.DefaultRequestHeaders.Add("Accept-Language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7");
+        client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+        client.DefaultRequestHeaders.Add("DNT", "1");
+        client.DefaultRequestHeaders.Add("Connection", "keep-alive");
+        client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+        client.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
+        client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
+        client.DefaultRequestHeaders.Add("Sec-Fetch-Site", "none");
+        client.DefaultRequestHeaders.Add("Sec-Fetch-User", "?1");
+        client.DefaultRequestHeaders.Add("Cache-Control", "max-age=0");
+
+        // Referer simulando acesso do Google (às vezes ajuda)
+        client.DefaultRequestHeaders.Add("Referer", "https://www.google.com/");
 
         return client;
     }
@@ -101,6 +109,9 @@ public class Request
     public async Task<string> GetHtmlAsync(string url)
     {
         using var client = CreateClient();
+
+        await Task.Delay(Random.Shared.Next(500, 1500));
+
         using var resp = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
 
         if (!resp.IsSuccessStatusCode)
